f449eb358fabb26575f025d0f6465d00b3227f22
nos-tromo <91366661+nos-tromo@users.noreply.github.com>
2025-06-10 00:13:53 +0200

    Reorder imports in __init__.py for improved organization

--- nextext/modules/__init__.py
+++ nextext/modules/__init__.py
@@ -2,10 +2,9 @@ from .ollama_cfg import call_ollama_server, text_summarization_prompt
 from .processing import FileProcessor
 from .topics import TopicModeling
 from .toxicity import ToxClassifier
-from .translation import Translator
 from .transcription import WhisperTranscriber
+from .translation import Translator
 from .words import WordCounter
-
 
 __all__ = [
     "call_ollama_server",
