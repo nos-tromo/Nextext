cedaa7752093fbaddff6377f2ea37eab51918064
theleastlikely <maskedsamurai@proton.me>
2025-06-10 14:14:27 +0200

    Refactor __init__.py to update import statements and  __all__  with adjustments from mappings_loader

--- nextext/utils/__init__.py
+++ nextext/utils/__init__.py
@@ -1,10 +1,11 @@
-from .mappings_loader import load_mappings
 from .logging_cfg import setup_logging
+from .mappings_loader import kv_to_vk, load_and_sort_mappings, load_mappings
 from .spacy_model_loader import download_model
-
 
 __all__ = [
     "setup_logging",
+    "load_and_sort_mappings",
+    "kv_to_vk",
     "load_mappings",
     "download_model",
 ]
