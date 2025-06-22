b3926dba1333051d164f047d97393c5a51bd9a20
nos-tromo <91366661+nos-tromo@users.noreply.github.com>
2025-06-22 20:56:49 +0200

    Update imports in __init__.py to include logging_cfg_loader and load_font_file

--- nextext/utils/__init__.py
+++ nextext/utils/__init__.py
@@ -1,10 +1,12 @@
-from .logging_cfg import setup_logging
+from .font_loader import load_font_file
+from .logging_cfg_loader import setup_logging
 from .mappings_loader import kv_to_vk, load_and_sort_mappings, load_mappings
 from .spacy_model_loader import download_model
 
 __all__ = [
     "setup_logging",
     "load_and_sort_mappings",
+    "load_font_file",
     "kv_to_vk",
     "load_mappings",
     "download_model",
