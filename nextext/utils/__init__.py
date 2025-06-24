0fda0ddf41ba83045f4247ea9bae30a8709da16b
nos-tromo <91366661+nos-tromo@users.noreply.github.com>
2025-06-25 00:29:00 +0200

    Fix import statement for downloading spaCy model in utils module

--- nextext/utils/__init__.py
+++ nextext/utils/__init__.py
@@ -1,7 +1,7 @@
 from .font_loader import load_font_file
 from .logging_cfg_loader import setup_logging
 from .mappings_loader import kv_to_vk, load_and_sort_mappings, load_mappings
-from .spacy_model_loader import download_model
+from .tokenizer_loader import download_spacy_model
 
 __all__ = [
     "setup_logging",
@@ -9,5 +9,5 @@ __all__ = [
     "load_font_file",
     "kv_to_vk",
     "load_mappings",
-    "download_model",
+    "download_spacy_model",
 ]
